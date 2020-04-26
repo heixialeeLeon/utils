526d24476d576e13fba8fad23ff92dbc62f16ee0
lipeizhao <lipeizhao@encootech.com>
2020-04-26 15:28:25 +0800

    to show color and gray images

--- img/view.py
+++ img/view.py
@@ -6,15 +6,19 @@ from skimage import util
 DIRECTION_HORIZONTAL = 0
 DIRECTION_VERTICAL =1
 
-def show_tensor(*tensors, dim=4,direction=0, timeout=1000):
+def show_tensor(tensors, direction=0, timeout=1000):
     is_first_image = True
     show_img = None
     for data in tensors:
-        if dim == 4:
+        print(data.shape)
+        if data.ndim == 4:
             data = torch.squeeze(data).data.cpu().numpy().transpose(1, 2, 0)
-        elif dim ==3:
+            channel = data.shape[1]
+        elif data.ndim ==3:
             data = data.cpu().numpy().transpose(1,2,0)
-        data = cv2.cvtColor(data, cv2.COLOR_RGB2BGR)
+            channel = data.shape[0]
+        if channel == 3:
+            data = cv2.cvtColor(data, cv2.COLOR_RGB2BGR)
         if is_first_image:
             show_img = data
             is_first_image = False
